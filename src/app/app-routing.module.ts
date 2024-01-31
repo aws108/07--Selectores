85a7fe532c82ed5b89c59acb1133050d1679cd0a
aws108 <sergiolizanagarcia@gmail.com>
2024-01-31 19:28:19 +0100

    lazy loading, crear estructura proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ // Lazy loading
+  {
+    path: 'selector',
+    loadChildren: () => import ('./countries/countries.module').then(m =>m.CountriesModule), // lazyLoad. Path del módulo hijo de app
+  },
+  {
+    path: '**',
+    redirectTo: 'selector',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
